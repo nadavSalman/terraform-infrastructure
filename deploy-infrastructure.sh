5106661673cf239053866e07d46bb957b2bd4d5b
Nadav Salman <asalman@attentigroup.com>
2021-06-03 11:40:33 +0300

    update .gitignore

---
+++ deploy-infrastructure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source conf.env
+
+echo "      #  Start deploy infrastructure on Azure Cloude  # "
+
+#terraform apply -var "admin_username=$USERNAME" -var "admin_password=$PASSWORD" --auto-approve
+
+echo "      #  End deploy infrastructure on Azure Cloude  #"
